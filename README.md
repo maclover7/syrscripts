54cfe1b00cec766e93fdc0940f4c111bc212cefc
Jon Moss <jmoss@syracuse.com>
2024-10-20 19:12:58 -0400

    Create county chunks of NY 22

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ### Projects
 
+- `district-to-county`: Converts a district boundary into several county-sized
+features.
 - `redistricting-2024`: Compare the boundaries of the 22nd Congressional district
 between the Cervas plan implemented in 2022 and the plan proposed in 2024 by the
 Independent Redistricting Commission.
---
+++ district-to-county/district-to-county.py
@@ -0,0 +1,25 @@
+import geopandas as gpd
+import requests
+
+### Import counties
+counties = 'https://gisservices.its.ny.gov/arcgis/rest/services/NYS_Civil_Boundaries/FeatureServer/2/query?where=1%3D1&outSR=4326&f=geojson'
+counties = requests.get(counties)
+counties = counties.json()
+counties = gpd.GeoDataFrame.from_features(counties, crs='EPSG:4326')
+
+### Import congressional districts
+districts = gpd.read_file('./ny-c.geojson')
+districts = districts[['NAME', 'geometry']]
+
+### Merge counties, selected districts
+SELETED_DISTRICTS = [{ 'type': 'C', 'number': '22' }]
+for selected_district in SELETED_DISTRICTS:
+  district = districts[districts.NAME == selected_district['number']]
+  result = gpd.overlay(district, counties)
+
+  result = result[['NAME_2', 'geometry']]
+  result = result.rename(columns={'NAME_2': 'name'})
+  result['name'] = result.name.str.capitalize() + ' County'
+
+  filename = 'dist-%s-%s.geojson' % (selected_district['type'], selected_district['number'])
+  result.to_file(filename, driver='GeoJSON')
