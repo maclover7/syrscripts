b148473f241afb14503597700d44864fdd983400
Jon Moss <jmoss@syracuse.com>
2024-10-23 15:56:51 -0400

    Import Assembly, Senate district shapefiles

--- district-to-county/district-to-county.py
+++ district-to-county/district-to-county.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import pandas as pd
 import requests
 
 ### Import counties
@@ -7,19 +8,32 @@ counties = requests.get(counties)
 counties = counties.json()
 counties = gpd.GeoDataFrame.from_features(counties, crs='EPSG:4326')
 
-### Import congressional districts
-districts = gpd.read_file('./ny-c.geojson')
-districts = districts[['NAME', 'geometry']]
+### Import districts
+CHAMBERS = ['C', 'SA', 'SS']
+districts = pd.DataFrame()
+
+for chamber in CHAMBERS:
+  file_districts = gpd.read_file('./ny-%s.geojson' % chamber.lower())
+  file_districts['chamber'] = chamber
+  districts = pd.concat([districts, file_districts[['NAME', 'chamber', 'geometry']]])
 
 ### Merge counties, selected districts
-SELETED_DISTRICTS = [{ 'type': 'C', 'number': '22' }, { 'type': 'C', 'number': '24' }]
+SELETED_DISTRICTS = [
+  { 'chamber': 'C', 'number': '22' },
+  { 'chamber': 'C', 'number': '24' },
+  { 'chamber': 'SS', 'number': '50' }
+]
+
 for selected_district in SELETED_DISTRICTS:
-  district = districts[districts.NAME == selected_district['number']]
+  district = districts[
+    (districts.chamber == selected_district['chamber']) &
+    (districts.NAME == selected_district['number'])
+  ]
   result = gpd.overlay(district, counties)
 
   result = result[['NAME_2', 'geometry']]
   result = result.rename(columns={'NAME_2': 'name'})
   result['name'] = result.name.str.capitalize() + ' County'
 
-  filename = 'dist-%s-%s.geojson' % (selected_district['type'], selected_district['number'])
+  filename = 'dist-%s-%s.geojson' % (selected_district['chamber'], selected_district['number'])
   result.to_file(filename, driver='GeoJSON')
